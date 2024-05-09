f4b0ad78e60c65e861beb424311af5f86f3125dd
Spencer Faith <13sfaith@gmail.com>
2024-05-09 17:22:55 -0400

    update init for client server merge

--- web-app/level-2/test_init.sh
+++ web-app/level-2/test_init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 
-tar -xvf ./server-1.0.0.tgz
+# tar -xvf ./server-1.0.0.tgz
 
 pushd package
 sudo mkdir /data
 sudo mkdir /data/db
 sudo mongod &
 cp -r /usr/lib/node_modules node_modules
-nodemon index.js
+# nodemon index.js
