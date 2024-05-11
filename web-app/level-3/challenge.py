6aaa8cfbaf2286df6ec62f78cdaaf824125c0c86
Adil Ahmad <adilahmad17@users.noreply.github.com>
2024-05-11 11:29:14 -0700

    Update challenge.py

--- web-app/level-3/challenge.py
+++ web-app/level-3/challenge.py
@@ -1,8 +1,12 @@
-plaintext = "party on main street"
+print ("Desert CodeSprouts (Task 3)")
+
+fp = open ("/home/hacker/forbidden/plaintext")
+plaintext = fp.read()
 user_plaintext = input('What is the plaintext?\n')
 
 if (plaintext == user_plaintext):
     f = open("/home/hacker/forbidden/flag", "r")
+    print ("Correct!")
     print ("The flag is: ", f.read())
 else:
-    print ("Sorry, the plaintext is wrong. Try again!\n")
+    print ("Sorry, that's wrong. Please try again!\n")
