17779772de017be12835bb7f30e42bee0a03e66c
Adil Ahmad <adilahmad17@users.noreply.github.com>
2024-05-11 11:31:50 -0700

    Update challenge.py

--- web-app/level-3/challenge.py
+++ web-app/level-3/challenge.py
@@ -2,7 +2,7 @@ print ("Desert CodeSprouts (Task 3)")
 
 fp = open ("/home/hacker/forbidden/plaintext", "r")
 plaintext = fp.read()
-user_plaintext = input('What is the plaintext? (all lower letters and no "!") \n')
+user_plaintext = input('What is the plaintext? (all lowercase letters and no "!") \n')
 
 if (plaintext == user_plaintext):
     f = open("/home/hacker/forbidden/flag", "r")
