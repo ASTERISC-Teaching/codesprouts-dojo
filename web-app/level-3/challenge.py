dcf2a938f5759f3869446b1c2f0efff792751df0
Adil Ahmad <adilahmad17@users.noreply.github.com>
2024-05-11 11:10:32 -0700

    Update challenge.py

--- web-app/level-3/challenge.py
+++ web-app/level-3/challenge.py
@@ -1,1 +1,8 @@
-print("put your code here Adil!")
+plaintext = "party on main street"
+user_plaintext = input('What is the plaintext?\n')
+
+if (plaintext == user_plaintext):
+    f = open("/home/hacker/forbidden/flag", "r")
+    print ("The flag is: ", f.read())
+else:
+    print ("Sorry, the plaintext is wrong. Try again!\n")
