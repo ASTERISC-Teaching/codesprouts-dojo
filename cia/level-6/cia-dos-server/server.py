46611965340a9b494688c4fbc620a44987d74a07
rahulpriolkar <rahulpriolkar01@gmail.com>
2025-05-15 17:50:25 -0700

    Added 3 challenges

--- cia/level-6/cia-dos-server/server.py
+++ cia/level-6/cia-dos-server/server.py
@@ -2,28 +2,30 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 
 # Global counter for GET requests
 get_request_count = 0
+THRESHOLD = 1000
 
 class RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        print("received request")
         global get_request_count
         get_request_count += 1
+        print("received request - ", get_request_count)
 
-        response_body = b"Response OK"
+        if get_request_count < THRESHOLD:
+            response_body = b"Response OK"
+        else:
+            response_body = b"SUCCESS"
 
         self.send_response(200)
         self.send_header("Content-Type", "text/plain")
         self.send_header("Content-Length", str(len(response_body)))
         self.send_header("Connection", "close")
+        self.send_header("Access-Control-Allow-Origin", "*")
         self.end_headers()
 
         self.wfile.write(response_body)
         self.wfile.flush()
 
-        # Response content
-        # self.wfile.write(response.encode('utf-8'))
-
-def run(server_class=HTTPServer, handler_class=RequestHandler, port=8000):
+def run(server_class=HTTPServer, handler_class=RequestHandler, port=8080):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
     print(f"Serving on port {port}...")
